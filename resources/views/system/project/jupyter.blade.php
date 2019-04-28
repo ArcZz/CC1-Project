218286b80688364de29eb7c3ecbd24b695caa853
Ryan Lokugamage <erangal@outlook.com>
2019-04-27 21:20:36 -0500

    integrated old jupyter notebooks UI with new UI layer

--- resources/views/system/project/jupyter.blade.php
+++ resources/views/system/project/jupyter.blade.php
@@ -4,20 +4,19 @@
 <section class="bg-light" id="services">
         <div class="container">
             <div class="row">
+                <a href="{{ url("/system/recommender/recommender") }}" ><--Back to Recommender</a>
+            </div>
+            <div class="row">
                 <div class="col-lg-12">
                     <h3 class="section-heading">Jupyter Recommendation System</h3>
                     <hr class="hr-line">
                     <!-- <h3 class="section-subheading"> -->
                     <p>List of Recommended Notebooks</p>
                     <p></p>
-                    <br>
-
+                    </br>
                 </div>
             </div>
-            
-
-            <br>
-
+            </br>
             @php
                 // echo $notebooks;
                $jsonData = json_decode($notebooks,true);
@@ -29,9 +28,9 @@
                     echo '<br><br>';
                }
             @endphp
-
         </div>
-    </section>
+    </div>
+</section>
 
 @endsection
 
--- resources/views/system/recommender/jupyter_recommender.blade.php
+++ resources/views/system/recommender/jupyter_recommender.blade.php
@@ -1,17 +1,20 @@
-
-   <div class="row">
-      <div class="col-12">
-         <h3 class="section-heading">Jupyter Notebook Recommender</h3>
-         <hr>
-      </div>
+<div class="row">
+   <div class="col-12">
+      <h3 class="section-heading">Jupyter Notebook Recommender</h3>
+      <hr>
    </div>
-   <div class="row"  >
-      <div class="col-md-12">
-
-
-
-         
-           
-      </div>
-   </div>
+</div>
+<div class="row">
+   <div class="col-md-12">
+      <form action="{{ url("/project/jupyter") }}" method="get">
+         {{ csrf_field() }}
+         <div class="form-group">
+            <label for="juin">Jupyter Notebook Input</label>
+            <input type="text" name="jupInput" class="form-control" id="juin" aria-describedby="juHelp" placeholder="Enter a keyword">
+            </br>
+            <button type="submit" class="btn btn-primary ">Confirm</button>
+         </div>
+      </form>
+   </div> 
+</div>
  
