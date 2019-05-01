82afe15e15fff9d319905d9fe7d5a0343eede786
KomalVekaria0309 <47259148+KomalVekaria0309@users.noreply.github.com>
2019-05-01 17:59:38 -0500

    Cloud solution template

--- resources/views/system/recommender/template_recommender.blade.php
+++ resources/views/system/recommender/template_recommender.blade.php
@@ -13,11 +13,9 @@
             <div  class="col-md-12" id="firstSection" ng-show="visibleSection && visibleSection=='firstpage'">
                <h4 style="display: inline-block;">Step 1: Image processing analysis - Choose Image</h4>
                <p style="text-align: justify;">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  Image processing analysis is one example of how cloud solution template recommender works.
                </p>
-               <p style="text-align: justify;">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-               </p>
+              
                <form type="POST">
                   <p style="text-align: justify;">
                      <label>Choose Image &nbsp;&nbsp;</label><input type="file" name="img"/>
