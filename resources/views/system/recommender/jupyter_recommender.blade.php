4db92beafc1a1c7f9e90f0a3ec6add3066ebab0d
Ryan Lokugamage <erangal@outlook.com>
2019-05-06 18:20:09 -0500

    reworking to use ajax instead of php

--- resources/views/system/recommender/jupyter_recommender.blade.php
+++ resources/views/system/recommender/jupyter_recommender.blade.php
@@ -1,3 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
 <div class="row">
    <div class="col-12">
       <h3 class="section-heading">Jupyter Notebook Recommender</h3>
@@ -6,7 +9,7 @@
 </div>
 <div class="row">
    <div class="col-md-12">
-      <form action="{{ url("/project/jupyter") }}" method="get">
+      <form  method="get" id="juform">
          {{ csrf_field() }}
          <div class="form-group">
             <label for="juin">Jupyter Notebook Input</label>
@@ -17,4 +20,38 @@
       </form>
    </div> 
 </div>
+<div class="row">
+   <div class="col-md-12">
+         <div id="results">
+   
+         </div>
+   </div>
+</div>
+
+<script>
+   function parseInput(input) {
+      return input.toString().replace(/ /g, "%20")
+   }
+
+
+   $(document).ready(function() {
+      $('#juform').on('submit', function(event) {
+         var input = parseInput($('#juin'))
+
+         var url = "http://ec2-3-94-109-60.compute-1.amazonaws.com:8001/api/rec/jupyter?keyword=" + input
+
+         $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'JSON',
+            success: function(data) {
+               var html = ""
+               var notebooks = data['notebooks']
+               
+               $('#results').html(notebooks.toString())
+            }
+         })
+      })
+   })
+</script>
 
